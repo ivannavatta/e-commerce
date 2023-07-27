c9a48bd5814fa2db937afb570a0d803a61013399
ivan navatta <ivannavatta@gmail.com>
2023-07-27 02:12:09 -0300

    correcciones

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// const productos = [
-//     {id: 1, producto: "Play 5"},
-//     {id: 2, producto: "Fifa 23"},
-//     {id: 3, producto: "Auriculares play 5"}
-// ]
 
 function play(){
     let resultado = 0
@@ -17,10 +12,22 @@ function play(){
        
         alert(`Play 5: ${precio_play} dolares 
         Con iva: ${resultado} dolares`)
-        const descuento = prompt("Agrega codigo de descuento para un 20% OFF").toLowerCase()
-        if (descuento == "banana" || descuento == "sandia"){
-            let resultadoConDescuento = resultado * 0.8
-            alert(`Total con descuento aplicado: ${resultadoConDescuento} dolares`)
+        class Play {
+            constructor(nombre, precio){
+                this.nombre = nombre 
+                this.precio = precio
+            }
+            descuento(){
+                this.precio = this.precio * 0.8;
+            }
+            
+        }
+        const play = []
+        play.push(new Play("Play 5", 500))
+
+        for (producto of play){
+            producto.descuento();
+            alert(`Total con 20% OFF: ${producto.precio} dolares`)
         }
         rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
     }while(rta != "salir")
@@ -31,5 +38,88 @@ function play(){
 }
 function button(){
     play();
+}
+
+
+function fifa(){
+    let resultado = 0
+   let precio_fifa = 50
+   
+    function iva(){
+        return precio_fifa * 0.6
+    }
+    do{
+
+        resultado =  precio_fifa + iva()
+       
+        alert(`Fifa 23: ${precio_fifa} dolares 
+        Con iva: ${resultado} dolares`)
+        class Fifa {
+            constructor(nombre, precio){
+                this.nombre = nombre 
+                this.precio = precio
+            }
+            descuento(){
+                this.precio = this.precio * 0.8;
+            }
+            
+        }
+        const fifa = []
+        fifa.push(new Fifa("FIfa 23", 50))
+
+        for (producto of fifa){
+            producto.descuento();
+            alert(`Total con 20% OFF: ${producto.precio} dolares`)
+        }
+        rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
+    }while(rta != "salir")
+
+ 
+    
+
+}
+function button2(){
+    fifa();
+}
+
+function auris(){
+    let resultado = 0
+   let precio_auris = 300
+   
+    function iva(){
+        return precio_auris * 0.6
+    }
+    do{
+
+        resultado =  precio_auris + iva()
+       
+        alert(`Auriculares play 5: ${precio_auris} dolares 
+        Con iva: ${resultado} dolares`)
+        class Auris {
+            constructor(nombre, precio){
+                this.nombre = nombre 
+                this.precio = precio
+            }
+            descuento(){
+                this.precio = this.precio * 0.8;
+            }
+            
+        }
+        const auris = []
+        auris.push(new Auris("Auriculares play 5", 300))
+
+        for (producto of auris){
+            producto.descuento();
+            alert(`Total con 20% OFF: ${producto.precio} dolares`)
+        }
+        rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
+    }while(rta != "salir")
+
+ 
+    
+
+}
+function button3(){
+    auris();
 }
 
