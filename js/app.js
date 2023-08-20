4dc8280a9d71b1ba3a88eda2460e04a29b4626d8
ivan navatta <ivannavatta@gmail.com>
2023-08-20 15:01:16 -0300

    agregado de funciones al carrito y nuevos html

--- js/app.js
+++ js/app.js
@@ -1,145 +1,4 @@
 
-// function play(){
-//     let resultado = 0
-//    let precio_play = 500
-   
-//     function iva(){
-//         return precio_play * 0.6
-//     }
-//     do{
-
-//         resultado =  precio_play + iva()
-       
-//         alert(`Play 5: ${precio_play} dolares\nCon iva: ${resultado} dolares`)
-//         class Play {
-//             constructor(nombre, precio){
-//                 this.nombre = nombre 
-//                 this.precio = precio
-//             }
-//             descuento(){
-//                 this.precio = (this.precio + iva()) * 0.8;
-//             }
-            
-//         }
-//         const play = []
-//         play.push(new Play("Play 5", 500))
-      
-
-//         for (producto of play){
-//             let descuento = "Colocar la palabra 'descuento' para 20% OFF"
-//             if(prompt(descuento).toLowerCase() == "descuento"){
-//             producto.descuento();
-//             alert(`Total con 20% OFF: ${producto.precio} dolares`)
-//         }
-//         else{
-//             alert(`Lo siento no obtuviste el descuento el precio final es:\nTotal: ${resultado} dolares`)
-//         }
-//     }
-    
-//         rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
-//     }while(rta != "salir")
-
- 
-    
-
-// }
-// function button(){
-//     play();
-// }
-
-
-// function returnal(){
-//     let resultado = 0
-//    let precio_returnal = 50
-   
-//     function iva(){
-//         return precio_returnal * 0.6
-//     }
-//     do{
-
-//         resultado =  precio_returnal + iva()
-       
-//         alert(`Returnal: ${precio_returnal} dolares\nCon iva: ${resultado} dolares`)
-//         class Returnal {
-//             constructor(nombre, precio){
-//                 this.nombre = nombre 
-//                 this.precio = precio
-//             }
-//             descuento(){
-//                 this.precio = (this.precio + iva()) * 0.8;
-//             }
-            
-//         }
-//         const returnal = []
-//         returnal.push(new Returnal("Returnal", 50))
-
-//         for (producto of returnal){
-//            let descuento = "Colocar la palabra 'descuento' para 20% OFF"
-//             if(prompt(descuento).toLowerCase() == "descuento"){
-                
-//             producto.descuento();
-//             alert(`Total con 20% OFF: ${producto.precio} dolares`)
-//         }
-//         else{
-//             alert(`Lo siento no obtuviste el descuento el precio final es:\nTotal: ${resultado} dolares`)
-//         }
-//     }
-//         rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
-//     }while(rta != "salir")
-
- 
-    
-
-// }
-// function button2(){
-//     returnal();
-// }
-
-// function auris(){
-//     let resultado = 0
-//    let precio_auris = 300
-   
-//     function iva(){
-//         return precio_auris * 0.6
-//     }
-//     do{
-
-//         resultado =  precio_auris + iva()
-       
-//         alert(`Auriculares play 5: ${precio_auris} dolares\nCon iva: ${resultado} dolares`)
-//         class Auris {
-//             constructor(nombre, precio){
-//                 this.nombre = nombre 
-//                 this.precio = precio
-//             }
-//             descuento(){
-//                 this.precio = (this.precio + iva()) * 0.8;
-//             }
-            
-//         }
-//         const auris = []
-//         auris.push(new Auris("Auriculares play 5", 300))
-
-//         for (producto of auris){
-//             let descuento = "Colocar la palabra 'descuento' para 20% OFF"
-//             if(prompt(descuento).toLowerCase() == "descuento"){
-//             producto.descuento();
-//             alert(`Total con 20% OFF: ${producto.precio} dolares`)
-//         }
-//         else{
-//             alert(`Lo siento no obtuviste el descuento el precio final es:\nTotal: ${resultado} dolares`)
-//         }
-//     }
-//         rta = prompt("Escriba 'salir' para finalizar el pedido").toLowerCase()
-//     }while(rta != "salir")
-
- 
-    
-
-// }
-// function button3(){
-//     auris();
-// }
 
 
 //carrito vacio para agrear las cosas luego
@@ -168,6 +27,9 @@ const valorTotal = document.querySelector('.total-pagar')
 const countProducts = document.querySelector('#contador-productos')
 
 const btnComprar = document.querySelector('.bubbly-button')
+
+
+
 
 //evnto para guardar el carrito cuando se recare la web
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -285,6 +147,7 @@ function leerDatosProducto(i){
 
 // mostrar los datos del producto
 function carritoHTML(){
+
  
     let total = 0;
 	let totalOfProducts = 0;
@@ -294,13 +157,40 @@ function carritoHTML(){
         fila.innerHTML = `
           <td class='img-pro-carrito'> <img src= "${producto.img}"  width="125"/> </td>
           <td>   ${producto.title}   </td>
-          <td class='price-pro-carrito'>   ${producto.price}   </td>
-          <td class='cantidad-pro-carrito'>   ${producto.cantidad}   </td>
+          <td class='price-pro-carrito'>  $${parseInt(producto.cantidad * producto.price.slice(1))}   </td>
+         
+
+          
+          <td class='cantidad-pro-carrito'> <span class='quantity-btn-decrese'>-</span> ${producto.cantidad} <span class='quantity-btn-increse'>+</span></td>
+          
+          
+      
+          
+          
           <td> 
           <a href="#" class="borrarProducto" data-id="${producto.id}"> ❌ </a>
           </td>
         `;
         contenedorCarrito.appendChild(fila)
+
+        const decrese = fila.querySelector('.quantity-btn-decrese')
+        decrese.addEventListener('click', ()=>{
+            if(producto.cantidad !== 1){
+                producto.cantidad --;
+            carritoHTML();
+
+            }
+            
+        })
+        const increse = fila.querySelector('.quantity-btn-increse')
+        increse.addEventListener('click', ()=>{
+        
+                producto.cantidad ++;
+            carritoHTML();
+
+            
+        })
+
         
         total = total + parseInt(producto.cantidad * producto.price.slice(1))
         totalOfProducts = totalOfProducts + producto.cantidad;
